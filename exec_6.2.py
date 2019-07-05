bd39618ad585ca4e2fba69c3391576b6b4411d6c
Daniel Bremer <5bremer@informatik.uni-hamburg.de>
2019-07-06 01:31:41 +0200

    WIP - Size somehow is just a quarter for submatrices

--- exec_6.2.py
+++ exec_6.2.py
@@ -2,7 +2,7 @@
 import sys
 import argparse
 import numpy as np
-import multiprocessing as mp
+import multiprocess as mp
 import math
 import shapely.geometry
 import matplotlib.pyplot as plt
@@ -18,6 +18,10 @@ R_1 = 50
 _O_2 = (250, 200)
 R_2 = 200
 
+_G_1 = (580, 150)
+_G_2 = (230, 470)
+_S = (900, 500)
+R_SG = 10
 
 def compute_fk(theta_1, theta_2, a_1, a_2):
     """ Compute forward kinematics for 2D planar manipulator. 
@@ -32,27 +36,33 @@ def compute_fk(theta_1, theta_2, a_1, a_2):
 
     return np.array([x, y])
 
-
 def tf_base_to_world(position):
     """ Transform base link coordinates to world coordinates. """
     return position + BASE_POS
 
 
-def calc_cspace(theta_1_fullrange, theta_2_fullrange, precision, c_obstacles, queue, id, numproc):
-    t1_start = math.floor(theta_1_fullrange / numproc) * id
-    t2_start = math.floor(theta_1_fullrange / numproc) * id
-    t1_range = math.floor(theta_1_fullrange / numproc) - 1
-    t2_range = math.floor(theta_1_fullrange / numproc) - 1
-    if id == numproc - 1:
-        t1_range = t1_range + 1
-        t2_range = t2_range + 1
+def main(args):
+    precision = args.precision
+    # Build C-Obstacles by adding radius of TCP to the
+    # obstacle radius
+    O_1 = shapely.geometry.Point(_O_1).buffer(R_1 + 2)
+    O_2 = shapely.geometry.Point(_O_2).buffer(R_2 + 2)
+    c_obstacles = [O_1, O_2]
 
-    print("Process %d: t1_start: %3d\tt1_range: %3d\tt2_start: %3d\tt2_range: %3d" % (id, t1_start, t1_range, t2_start, t2_range))
+    # Build the start point and two goal points
+    # substract radius of TCP so that TCP is fully in area
+    G_1 = shapely.geometry.Point(_G_1).buffer(R_SG - 2)
+    G_2 = shapely.geometry.Point(_G_2).buffer(R_SG - 2)
+    S = shapely.geometry.Point(_S).buffer(R_SG - 2)
 
-    c_space = np.zeros((theta_1_fullrange, theta_2_fullrange))
-    print(range(t2_start, t2_start + t2_range))
-    for theta_2 in range(t2_start, t2_start + t2_range):
-        for theta_1 in range(t1_start, t1_start + t1_range):
+    theta_1_range = int(360 / precision) + 1 # include upper bound
+    theta_2_range = int(360 / precision) + 1
+
+    # Build configuration space
+    # FIXME: Slow as fuck, it needs to be run on multiple processors
+    c_space = np.zeros((theta_1_range, theta_2_range))
+    for theta_2 in range(theta_2_range):
+        for theta_1 in range(theta_1_range):
             base_tcp = compute_fk(theta_1 * precision, theta_2 * precision, L_1, L_2)
             world_tcp = tf_base_to_world(base_tcp)
 
@@ -65,46 +75,7 @@ def calc_cspace(theta_1_fullrange, theta_2_fullrange, precision, c_obstacles, qu
             for c_obstacle in c_obstacles:
                 if c_obstacle.intersects(tcp):
                     c_space[theta_2][theta_1] = 0
-                    # print("Configuration collision: {}".format((theta_1 * precision, theta_2 * precision)))
-    queue.put((c_space))
-
-
-def main(args):
-    precision = args.precision
-    # Build C-Obstacles by adding radius of TCP to the
-    # obstacle radius
-    O_1 = shapely.geometry.Point(_O_1).buffer(R_1 + 2)
-    O_2 = shapely.geometry.Point(_O_2).buffer(R_2 + 2)
-    c_obstacles = [O_1, O_2]
-
-    theta_1_range = int(360 / precision) + 1 # include upper bound
-    theta_2_range = int(360 / precision) + 1
-
-    # Build configuration space
-    # FIXME: Slow as fuck, it needs to be run on multiple processors
-    c_space = np.zeros((theta_1_range, theta_2_range))
-    print(c_space)
-    queue = mp.Queue()
-    p0 = mp.Process(target=calc_cspace, args=(theta_1_range, theta_2_range, precision, c_obstacles, queue, 0, 4))
-    p1 = mp.Process(target=calc_cspace, args=(theta_1_range, theta_2_range, precision, c_obstacles, queue, 1, 4))
-    p2 = mp.Process(target=calc_cspace, args=(theta_1_range, theta_2_range, precision, c_obstacles, queue, 2, 4))
-    p3 = mp.Process(target=calc_cspace, args=(theta_1_range, theta_2_range, precision, c_obstacles, queue, 3, 4))
-    p0.start()
-    p1.start()
-    p2.start()
-    p3.start()
-    np.add(c_space, queue.get(), c_space)
-    #np.add(c_space, queue.get(), c_space)
-    #np.add(c_space, queue.get(), c_space)
-    #np.add(c_space, queue.get(), c_space)
-    queue.get()
-    queue.get()
-    queue.get()
-    p0.join()
-    p1.join()
-    p2.join()
-    p3.join()
-    print(c_space.shape)
+                    #print("Configuration collision: {}".format((theta_1 * precision, theta_2 * precision)))
 
     # Need to flip the array because numpy access them row first
     # are accessed row first.
@@ -135,4 +106,3 @@ if __name__ == "__main__":
     args = parser.parse_args() 
 
     main(args)
-
